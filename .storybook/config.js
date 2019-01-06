dc2ed1fc28cfd5d61fa05cd36b56ccdc092764b8
Bernabe <bernabe@maniak.com.mx>
2019-01-06 17:59:39 -0600

    Add info addon. Add it as global addon in config file.

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,8 @@
-import { configure, setAddon } from "@storybook/react";
+import { configure, setAddon, addDecorator } from "@storybook/react";
 import JSXAddon from "storybook-addon-jsx";
+import { withInfo } from "@storybook/addon-info";
 
+addDecorator(withInfo);
 setAddon(JSXAddon);
 // it looks through the src folder, for files that ends in .stories.js
 const req = require.context("../src", true, /.stories.js$/);
--- src/Button.stories.js
+++ src/Button.stories.js
@@ -1,7 +1,9 @@
 import React from "react";
-import { storiesOf } from "@storybook/react";
+import { addParameters, storiesOf } from "@storybook/react";
 import Button from "./Button";
 
-storiesOf("Button", module).addWithJSX("with background", () => (
-  <Button bg={"lightblue"}> I'm a button with blue background</Button>
-));
+storiesOf("Button", module)
+  .addParameters({ info: "This is a basic example" })
+  .addWithJSX("with background", () => (
+    <Button bg={"lightblue"}> I'm a button with blue background</Button>
+  ));
