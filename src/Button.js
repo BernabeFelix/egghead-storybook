0e58efa709fa06d2b16422e1ebf7fd51e3af0c38
Bernabe <bernabe@maniak.com.mx>
2019-01-06 17:58:56 -0600

    Add proptypes to Button

--- src/Button.js
+++ src/Button.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { string, oneOfType, arrayOf, node } from "prop-types";
 
 const Button = ({ bg, children }) => (
   <button style={{ backgroundColor: bg }}> {children} </button>
 );
+
+Button.propTypes = {
+  bg: string.isRequired,
+  children: oneOfType([node, arrayOf(node)])
+};
+
+Button.defaultProps = {
+  children: null
+};
 
 export default Button;
